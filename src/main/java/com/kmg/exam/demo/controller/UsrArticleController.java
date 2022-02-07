80f24f28232f4f25f5af5ff346bf78c040cb6d47
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-02-07 19:38:09 +0900

    article 테스트데이터 생성 구현

--- src/main/java/com/kmg/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/kmg/exam/demo/controller/UsrArticleController.java
@@ -9,30 +9,41 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.kmg.exam.demo.vo.Article;
 
-
-
 @Controller
 public class UsrArticleController {
 	List<Article> articles;
 	int articlesLastId;
-	
+
 	public UsrArticleController() {
 		articles = new ArrayList<>();
 		articlesLastId = 0;
+		makeTestData();
 	}
 	
+	private void makeTestData(){
+		for (int i=1; i<=10; i++){
+			int id = articlesLastId + 1;
+			articlesLastId = id;
+			String title = "제목"+i;
+			String body = "내용"+i;
+			Article article = new Article(id, title, body);
+			
+			articles.add(article);
+			
+		}
+	}
 	@RequestMapping("/usr/home/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
 		int id = articlesLastId + 1;
-		Article  article = new Article(id, title, body);
-		
+		Article article = new Article(id, title, body);
+
 		articlesLastId = id;
 		articles.add(article);
-		
+
 		return article;
 	}
-	
+
 	@RequestMapping("/usr/home/getArticles")
 	@ResponseBody
 	public List<Article> getArticles() {
