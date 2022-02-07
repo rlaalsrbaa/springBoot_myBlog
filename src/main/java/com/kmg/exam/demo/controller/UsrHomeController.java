4f5e71c4e07aee04ea579edd622d652bf34a44c9
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-02-07 18:30:45 +0900

    count의 값을 0으로 만드는 main4 구현

--- src/main/java/com/kmg/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/kmg/exam/demo/controller/UsrHomeController.java
@@ -35,4 +35,10 @@ public class UsrHomeController {
 	public int showMain4() {
 		return count++;
 	}
+	@RequestMapping("/usr/home/main5")
+	@ResponseBody
+	public String showMain5() {
+		count = 0;
+		return "count의 값이 0으로 초기화 됐습니다.";
+	}
 }
