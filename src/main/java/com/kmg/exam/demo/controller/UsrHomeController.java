138eaf44c71ec153e138fcdf74da674ca13fa2fc
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-02-07 18:50:11 +0900

    article이 있는 list 를 보여주는 getArticles 구현

--- src/main/java/com/kmg/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/kmg/exam/demo/controller/UsrHomeController.java
@@ -1,8 +1,14 @@
 package com.kmg.exam.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 @Controller
 public class UsrHomeController {
@@ -17,16 +23,25 @@ public class UsrHomeController {
 	public String showMain() {
 		return "안녕하세요.";
 	}
+	@RequestMapping("/usr/home/getArticles")
+	@ResponseBody
+	public List doGetArticles() {
+		Article article1 = new Article(1, "제목1");
+		Article article2 = new Article(2, "제목2");
+		
+		List<Article> list = new ArrayList<>();
+		list.add(article1);
+		list.add(article2);
+		
+		return list;
+	}
 
-	@RequestMapping("/usr/home/getCount")
-	@ResponseBody
-	public int getCount() {
-		return count;
-	}
-	@RequestMapping("/usr/home/doSetCount")
-	@ResponseBody
-	public String doSetCount(int count) {
-		this.count = count;
-		return "count의 값이 " +this.count +"으로 초기화 됐습니다.";
-	}
+	
+}
+
+@Data
+@AllArgsConstructor
+class Article{
+	private int id;
+	private String title;
 }
