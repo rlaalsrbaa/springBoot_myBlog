bb65986cbed0e37b80f1896af56e6c3810d30769
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-02-07 20:14:39 +0900

    게시물 삭제기능 구현

--- src/main/java/com/kmg/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/kmg/exam/demo/controller/UsrArticleController.java
@@ -19,33 +19,63 @@ public class UsrArticleController {
 		articlesLastId = 0;
 		makeTestData();
 	}
-	
-	private void makeTestData(){
-		for (int i=1; i<=10; i++){
-			String title = "제목"+i;
-			String body = "내용"+i;
+
+	private void makeTestData() {
+		for (int i = 1; i <= 10; i++) {
+			String title = "제목" + i;
+			String body = "내용" + i;
 			writeArticle(title, body);
 		}
 	}
-	@RequestMapping("/usr/home/doAdd")
-	@ResponseBody
-	
+
 	public Article writeArticle(String title, String body) {
 		int id = articlesLastId + 1;
 		Article article = new Article(id, title, body);
-		
+
 		articles.add(article);
 		articlesLastId = id;
 		return article;
 	}
-	
-	public Article doAdd(String title, String body) {
-		return writeArticle(title, body);
+
+	public void deleteArticle(int id) {
+		Article article = getArticle(id);
+
+		articles.remove(article);
 	}
 
+	private Article getArticle(int id) {
+		for (Article article : articles) {
+			if (article.getId() == id) {
+				return article;
+			}
+		}
+		return null;
+	}
+	
 	@RequestMapping("/usr/home/getArticles")
 	@ResponseBody
 	public List<Article> getArticles() {
 		return articles;
 	}
+	
+
+	@RequestMapping("/usr/home/doDelete")
+	@ResponseBody
+	public String doDelete(int id) {
+		Article article = getArticle(id);
+	
+		if ( article == null ) {
+			return id + "번 게시물이 존재하지 않습니다.";
+		}
+		deleteArticle(id);
+		
+		return id + "번 게시물이 삭제됐습니다.";
+	}
+	
+	@RequestMapping("/usr/home/doAdd")
+	@ResponseBody
+	public Article doAdd(String title, String body) {
+		return writeArticle(title, body);
+	}
+	
 }
