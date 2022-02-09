58cb774127f9fffd03c8521039cf324142a62db1
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-02-09 10:45:07 +0900

    getArticle 액션 구현

--- src/main/java/com/kmg/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/kmg/exam/demo/controller/UsrArticleController.java
@@ -65,6 +65,17 @@ public class UsrArticleController {
 		return articles;
 	}
 
+	@RequestMapping("/usr/home/getArticle")
+	@ResponseBody
+	public Object getArticleAction(int id) {
+		Article article = getArticle(id);
+		if (article == null) {
+			return id + "번 게시물이 존재하지 않습니다.";
+		}
+		return article;
+	}
+
+	
 	@RequestMapping("/usr/home/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
