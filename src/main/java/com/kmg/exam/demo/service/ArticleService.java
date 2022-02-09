394da5363ffb3e5ba9a44f71ed8ae9614d854a4c
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-02-09 11:33:45 +0900

    ArticleService에 있던 단순 저장관련 로직은 ArticleRepository로 이동

--- src/main/java/com/kmg/exam/demo/service/ArticleService.java
+++ src/main/java/com/kmg/exam/demo/service/ArticleService.java
@@ -1,65 +1,38 @@
 package com.kmg.exam.demo.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.kmg.exam.demo.repository.ArticleRepository;
 import com.kmg.exam.demo.vo.Article;
 
 @Service
 public class ArticleService {
-	private List<Article> articles;
-	private int articlesLastId;
-
-	public ArticleService() {
-			articles = new ArrayList<>();
-			articlesLastId = 0;
-			makeTestData();
-		}
-
-	private void makeTestData() {
-		for (int i = 1; i <= 10; i++) {
-			String title = "제목" + i;
-			String body = "내용" + i;
-			writeArticle(title, body);
-		}
+	private ArticleRepository articleRepository;
+	
+	public ArticleService(ArticleRepository articleRepository) {
+		this.articleRepository = articleRepository;
+		articleRepository.makeTestData();
 	}
 
 	public Article writeArticle(String title, String body) {
-		int id = articlesLastId + 1;
-		Article article = new Article(id, title, body);
-
-		articles.add(article);
-		articlesLastId = id;
-		return article;
-	}
-
-	public void modifyArticle(int id, String title, String body) {
-		Article article = getArticle(id);
-
-		article.setTitle(title);
-		article.setBody(body);
-	}
-
-	public void deleteArticle(int id) {
-		Article article = getArticle(id);
-
-		articles.remove(article);
-	}
-
-	public Article getArticle(int id) {
-		for (Article article : articles) {
-			if (article.getId() == id) {
-				return article;
-			}
-		}
-		return null;
+		return articleRepository.writeArticle(title, body);
 	}
 
 	public List<Article> getArticles() {
-		return articles;
+		return articleRepository.getArticles();
 	}
 
+	public Article getArticle(int id) {
+		return articleRepository.getArticle(id);
+	}
+
+	public void deleteArticle(int id) {
+		articleRepository.deleteArticle(id);
+	}
+
+	public void modifyArticle(int id, String title, String body) {
+		articleRepository.modifyArticle(id, title, body);
+	}
 }
