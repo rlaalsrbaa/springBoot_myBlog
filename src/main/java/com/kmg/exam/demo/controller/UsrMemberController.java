aa58f98a973773c15c9c21c2a76693d2897ce936
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-02-10 12:36:40 +0900

    회원가입을 처리하기 위해, UsrMemberController, MemberService 추가

---
+++ src/main/java/com/kmg/exam/demo/controller/UsrMemberController.java
@@ -0,0 +1,25 @@
+package com.kmg.exam.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.kmg.exam.demo.service.MemberService;
+
+@Controller
+public class UsrMemberController {
+
+	@Autowired
+	MemberService memberService;
+	
+	@RequestMapping("/usr/member/join")
+	@ResponseBody
+	public String doJoin(String loginId, String loginPw, String name,
+			String nckname, String cellphonNo, String email) {
+		memberService.doJoin(loginId, loginPw, name,
+				nckname, cellphonNo, email);
+		return "안녕하세요.";
+	}
+
+}
---
+++ src/main/java/com/kmg/exam/demo/service/MemberService.java
@@ -0,0 +1,14 @@
+package com.kmg.exam.demo.service;
+
+import org.springframework.stereotype.Service;
+
+import com.kmg.exam.demo.repository.ArticleRepository;
+
+@Service
+public class MemberService {
+
+	public void doJoin(String loginId, String loginPw, String name, String nckname, String cellphonNo, String email) {
+		
+	}
+
+}
