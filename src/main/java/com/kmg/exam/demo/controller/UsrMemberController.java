82c55c54c0b82b1fef39621cd774b860ea8ab177
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-02-10 13:50:08 +0900

    회원가입시 입력데이터 유효성 체크

--- src/main/java/com/kmg/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/kmg/exam/demo/controller/UsrMemberController.java
@@ -13,18 +13,44 @@ public class UsrMemberController {
 
 	@Autowired
 	MemberService memberService;
-	
+
 	@RequestMapping("/usr/member/join")
 	@ResponseBody
-	public Object doJoin(String loginId, String loginPw, String name,
-			String nickname, String cellphoneNo, String email) {
-		int id = memberService.doJoin(loginId, loginPw, name,
-				nickname, cellphoneNo, email);
+	public Object doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
+			String email) {
+
+		if (loginId == null) {
+			return "loginId(을)를 입력해주세요.";
+		}
+
+		if (loginPw == null) {
+			return "loginPw(을)를 입력해주세요.";
+		}
+
+		if (name == null) {
+			return "name(을)를 입력해주세요.";
+		}
+
+		if (nickname == null) {
+			return "nickname(을)를 입력해주세요.";
+		}
+
+		if (cellphoneNo == null) {
+			return "cellphoneNo(을)를 입력해주세요.";
+		}
+
+		if (email == null) {
+			return "email(을)를 입력해주세요.";
+		}
+
+		int id = memberService.doJoin(loginId, loginPw, name, nickname, cellphoneNo, email);
+
 		if (id == -1) {
 			return "사용중인 로그인 아이디입니다.";
 		}
-		Member member = memberService.getMemberById(id);
 		
+		Member member = memberService.getMemberById(id);
+
 		return member;
 	}
 
