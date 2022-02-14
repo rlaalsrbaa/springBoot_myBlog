91c7b9836eee304610db92a277f28d982c5f89b5
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-02-15 01:28:58 +0900

    getArticle에 ResultData 적용

--- src/main/java/com/kmg/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/kmg/exam/demo/controller/UsrArticleController.java
@@ -1,6 +1,5 @@
 package com.kmg.exam.demo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +9,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.kmg.exam.demo.service.ArticleService;
 import com.kmg.exam.demo.vo.Article;
+import com.kmg.exam.demo.vo.ResultData;
+import com.kmg.exam.demo.util.Ut;
 
 @Controller
 public class UsrArticleController {
@@ -24,12 +25,12 @@ public class UsrArticleController {
 
 	@RequestMapping("/usr/article/getArticle")
 	@ResponseBody
-	public Object getArticle(int id) {
+	public ResultData getArticle(int id) {
 		Article article = articleService.getArticle(id);
 		if (article == null) {
-			return id + "번 게시물이 존재하지 않습니다.";
+			return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
 		}
-		return article;
+		return ResultData.from("S-1", Ut.f("%d번 게시물입니다.", id), article);
 	}
 
 	@RequestMapping("/usr/article/doAdd")
--- src/main/java/com/kmg/exam/demo/vo/ResultData.java
+++ src/main/java/com/kmg/exam/demo/vo/ResultData.java
@@ -13,6 +13,9 @@ public class ResultData {
 	private ResultData() {
 
 	}
+	public static ResultData from(String resultCode, String msg) {
+		return from(resultCode, msg, null);
+	}
 
 	public static ResultData from(String resultCode, String msg, Object data1) {
 		ResultData rd = new ResultData();
