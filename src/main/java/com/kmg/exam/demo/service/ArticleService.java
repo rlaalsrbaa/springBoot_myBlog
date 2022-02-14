8555cdd0692dd05c362895ac8382034052a2c607
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-02-15 01:31:36 +0900

    doAdd에 ResultData 적용

--- src/main/java/com/kmg/exam/demo/service/ArticleService.java
+++ src/main/java/com/kmg/exam/demo/service/ArticleService.java
@@ -1,6 +1,8 @@
 package com.kmg.exam.demo.service;
 
 import java.util.List;
+import com.kmg.exam.demo.util.Ut;
+import com.kmg.exam.demo.vo.ResultData;
 
 import org.springframework.stereotype.Service;
 
@@ -10,15 +12,18 @@ import com.kmg.exam.demo.vo.Article;
 @Service
 public class ArticleService {
 	private ArticleRepository articleRepository;
-	
+
 	public ArticleService(ArticleRepository articleRepository) {
 		this.articleRepository = articleRepository;
 	}
 
-	public int writeArticle(String title, String body) {
+	public ResultData writeArticle(String title, String body) {
 		articleRepository.writeArticle(title, body);
-		return articleRepository.getLastInsertId(); 
+		int id = articleRepository.getLastInsertId();
+
+		return ResultData.from("S-1", Ut.f("%d번 게시물이 생성되었습니다.", id), id);
 	}
+
 	public List<Article> getArticles() {
 		return articleRepository.getArticles();
 	}
