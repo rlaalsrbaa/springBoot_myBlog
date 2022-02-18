2eb2fa404852994398c1d6ca2a6f84cacdcff667
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-02-18 09:25:02 +0900

    jsp적용

--- src/main/java/com/kmg/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/kmg/exam/demo/controller/UsrHomeController.java
@@ -1,31 +1,20 @@
 package com.kmg.exam.demo.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.kmg.exam.demo.vo.Article;
-
-
+import com.kmg.exam.demo.service.MemberService;
 
 @Controller
 public class UsrHomeController {
-	private int count;
-
-	public UsrHomeController() {
-		count = 0;
-	}
-
 	@RequestMapping("/usr/home/main")
-	@ResponseBody
 	public String showMain() {
-		return "안녕하세요.";
+		return "usr/home/main";
 	}
-
-
-
 	
+	@RequestMapping("/")
+	public String showRoot() {
+		return "redirect:/usr/home/main";
+	}
 }
