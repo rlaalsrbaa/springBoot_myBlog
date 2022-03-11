d46a27fa9cac7774e0c4a2246672d6ec686ee58e
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-03-11 09:46:15 +0900

    로그인 처리에 rq적용

--- src/main/java/com/kmg/exam/demo/vo/Rq.java
+++ src/main/java/com/kmg/exam/demo/vo/Rq.java
@@ -19,18 +19,19 @@ public class Rq {
 
 	private HttpServletRequest req;
 	private HttpServletResponse resp;
+	private HttpSession session;
 
 	public Rq(HttpServletRequest req, HttpServletResponse resp) {
 		this.req = req;
 		this.resp = resp;
 
-		HttpSession httpSession = req.getSession();
+		this.session = req.getSession();
 		boolean isLogined = false;
 		int loginedMemberId = 0;
 
-		if (httpSession.getAttribute("loginedMemberId") != null) {
+		if (session.getAttribute("loginedMemberId") != null) {
 			isLogined = true;
-			loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
+			loginedMemberId = (int) session.getAttribute("loginedMemberId");
 		}
 
 		this.isLogined = isLogined;
@@ -61,5 +62,9 @@ public class Rq {
 
 	public void println(String str) {
 		print(str + "\n");
+	}
+
+	public void login(Member member) {
+		session.setAttribute("loginedMemberId", member.getId());
 	}
 }
