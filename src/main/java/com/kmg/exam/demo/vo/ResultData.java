44347a74ea29704a4bd3d5fddaffa8e20a97fd6a
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-03-14 11:35:53 +0900

    조회수 증가 성공에 대한 답장에 data2(글 번호)정보 추가

--- src/main/java/com/kmg/exam/demo/vo/ResultData.java
+++ src/main/java/com/kmg/exam/demo/vo/ResultData.java
@@ -13,14 +13,17 @@ public class ResultData<DT> {
 	private String data1Name;
 	@Getter
 	private DT data1;
-
+	@Getter
+	private String data2Name;
+	@Getter
+	private Object data2;
 
 	public static ResultData from(String resultCode, String msg) {
 		return from(resultCode, msg, null, null);
 	}
 
 	public static <DT> ResultData<DT> from(String resultCode, String msg, String data1Name, DT data1) {
-		ResultData<DT> rd = new ResultData<DT>();		
+		ResultData<DT> rd = new ResultData<DT>();
 		rd.resultCode = resultCode;
 		rd.msg = msg;
 		rd.data1Name = data1Name;
@@ -36,7 +39,13 @@ public class ResultData<DT> {
 	public boolean isFail() {
 		return isSuccess() == false;
 	}
+
 	public static <DT> ResultData<DT> newData(ResultData oldRd, String data1Name, DT newData) {
 		return from(oldRd.getResultCode(), oldRd.getMsg(), data1Name, newData);
+	}
+
+	public void setData2(String dataName, Object data) {
+		data2Name = dataName;
+		data2 = data;
 	}
 }
